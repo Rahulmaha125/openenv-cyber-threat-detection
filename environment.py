dc3e38686f3a507dd43e90e92b921d08bb9a39c7
Rahulmaha125 <rahulmahanavar125125125@gmail.com>
2026-04-11 11:29:29 +0530

    Update environment.py

--- environment.py
+++ environment.py
@@ -5,11 +5,11 @@ class CyberThreatEnv:
     def __init__(self):
 
         self.logs = [
-            ("20 failed login attempts from same IP", "block_ip"),
-            ("Login attempt from unusual country", "monitor"),
-            ("Normal user login activity", "allow"),
-            ("Multiple password reset attempts", "monitor"),
-            ("Suspicious brute force attack detected", "block_ip")
+            ("20 failed login attempts from same IP", "block_ip", 0.95),
+            ("Login attempt from unusual country", "monitor", 0.70),
+            ("Normal user login", "allow", 0.10),
+            ("Multiple password reset attempts", "monitor", 0.65),
+            ("Brute force attack detected", "block_ip", 0.98)
         ]
 
         self.current_log = None
@@ -19,20 +19,19 @@ class CyberThreatEnv:
         self.current_log = random.choice(self.logs)
 
         return {
-            "network_log": self.current_log[0]
+            "network_log": self.current_log[0],
+            "risk_score": self.current_log[2]
         }
 
     def step(self, action):
 
         correct_action = self.current_log[1]
 
-        if action == correct_action:
-            reward = 1.0
-        else:
-            reward = 0.0
+        reward = 1.0 if action == correct_action else 0.0
 
         done = True
 
         return {
-            "network_log": self.current_log[0]
-        }, reward, done, {}
+            "network_log": self.current_log[0],
+            "risk_score": self.current_log[2]
+        }, reward, done, {"correct_action": correct_action}
